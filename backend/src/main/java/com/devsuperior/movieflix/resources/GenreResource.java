16ebaebcf57cb40924149ccf455528d2603b6bfa
Márcio Gama <marciogama2000@gmail.com>
2021-08-04 11:59:57 -0300

    GenreDTO

--- backend/src/main/java/com/devsuperior/movieflix/resources/GenreResource.java
+++ backend/src/main/java/com/devsuperior/movieflix/resources/GenreResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.dto.GenreDTO;
 import com.devsuperior.movieflix.services.GenreService;
 
 @RestController
@@ -19,8 +19,8 @@ public class GenreResource {
 	private GenreService service;
 
 	@GetMapping
-	public ResponseEntity<List<Genre>> findAll(){
-		List<Genre> list = service.findAll();
+	public ResponseEntity<List<GenreDTO>> findAll(){
+		List<GenreDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ backend/src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -1,10 +1,13 @@
 package com.devsuperior.movieflix.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.movieflix.dto.GenreDTO;
 import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.repositories.GenreRepository;
 
@@ -14,7 +17,11 @@ public class GenreService {
 	@Autowired
 	private GenreRepository repository;
 	
-	public List<Genre> findAll() {
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<GenreDTO> findAll() {
+		
+		List<Genre> list = repository.findAll();
+		List<GenreDTO> listDto = list.stream().map(x -> new GenreDTO(x)).collect(Collectors.toList());
+		return listDto;
 	}
 }
